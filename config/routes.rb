4dd156faccb129189873f67b8894bc094cfde36f
rumikoacopa <rumikorios@gmail.com>
2020-11-11 14:03:46 -0500

    trys to change root to users#show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :drinks 
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "drinks#index"
+  root to: "users#show"
 end
