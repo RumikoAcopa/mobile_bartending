a461c3e10c35b872950a49aae8dfc64438a0a598
rumikoacopa <rumikorios@gmail.com>
2020-11-14 21:20:06 -0500

    modifies drinkorder model

--- app/models/drink_order.rb
+++ app/models/drink_order.rb
@@ -1,8 +1,7 @@
 class DrinkOrder < ApplicationRecord
   belongs_to :user
-  belongs_to :drink
+  belongs_to :drink #, counter_cache: :quantity_count
   
   validates :user_id, :drink_id, :special_instructions, :quantity, presence: true
 
-   
 end
