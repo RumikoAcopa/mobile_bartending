8ef98edc4e7dcb9f512fa6c824a30b82e278c066
rumikoacopa <rumikorios@gmail.com>
2020-11-14 21:20:55 -0500

    validates dob, age, validate age method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,24 @@ class User < ApplicationRecord
   has_many :drink_orders
   has_many :drinks, through: :drink_orders
   devise :database_authenticatable, :registerable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  validates :date_of_birth, :presence => true
+  validate :validate_age
+  private
+  def validate_age
+    if date_of_birth.present? && date_of_birth > 21.years.ago.to_date
+        errors.add(:date_of_birth, 'You should be over 21 years old.')
+    end
+  end
 
   #for omniauth
-
-
   #validates oauth
-    def self.from_google(uid:, email:, full_name:, avatar_url:)
-    user = User.find_or_create_by(email: email) do |u|
-      u.uid = uid
-      u.full_name = full_name
-      u.avatar_url = avatar_url
-      u.password = SecureRandom.hex
-    end
+  def self.from_google(uid:, email:, full_name:, avatar_url:)
+  user = User.find_or_create_by(email: email) do |u|
+    u.uid = uid
+    u.full_name = full_name
+    u.avatar_url = avatar_url
+    u.password = SecureRandom.hex
+  end
     user.update(uid: uid, full_name: full_name, avatar_url: avatar_url)
     user
   end
